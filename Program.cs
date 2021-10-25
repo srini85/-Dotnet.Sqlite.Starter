23b4f75c2cca1a36735a5488423355e2eb3a869a
Srini Vasudevan <srini@gofundraise.com>
2021-10-26 09:47:37 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dotnet.Sqlite.Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
